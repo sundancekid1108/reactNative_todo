c527b36714587b6ebb8269054e2eab206f4eb7a8
Mr. Sundance <sundancekid1108@gmail.com>
2018-12-29 23:39:31 +0900

    todo view 생성

--- react-todo/App.js
+++ react-todo/App.js
@@ -2,22 +2,49 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View, Text, Dimensions } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
-import { TextInput } from 'react-native-gesture-handler';
+import { TextInput, ScrollView } from 'react-native-gesture-handler';
+import ToDo from "./Todo"
 
 const { height, width } = Dimensions.get("window")
 
 export default class App extends React.Component {
+    state= {
+        newToDo: ""
+    };
+
+  
+
     render() {
+        const { newToDo } = this.state.newToDo;
         return ( 
             <View style = { styles.container }>
                 <StatusBar barStyle = "light-content" / >
                     <Text style = { styles.title } > Nice ToDo </Text>
                         <View style = { styles.card } >
-                            <TextInput style = { styles.input } placeholder = { "New To Do" } />
+                            <TextInput 
+                                style = { styles.input } 
+                                placeholder = { "New To Do" } 
+                                value = {newToDo}
+                                oneChangeText= {this._controlNewToDo}                        placeholderTextColor= {"#999"}
+                                returnKeyType={"done"}
+                                autoCorrect={false} >
+                            </TextInput>
+
+                            <ScrollView>
+                                <ToDo />
+                            </ScrollView>
                         </View>
             </View>
         );
+
+        _controlNewToDo = text => {
+            this.setState({
+              newToDo: text
+            });
+          };
     }
+ 
+
 }
 
 const styles = StyleSheet.create({
@@ -54,6 +81,11 @@ const styles = StyleSheet.create({
                 elevation: 3
               }
         })
-       
+    },
+    input: {
+        padding: 20,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: 1,
+        fontSize: 25
     }
 });
---
+++ react-todo/Todo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput} from "react-native";
+import PropTypes from "prop-types";
+
+export default class ToDo extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Hello I'm your To Do</Text>
+            </View>
+        );
+        
+    }
+}
+
+const styles = StyleSheet.create({})
