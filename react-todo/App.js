d602cba18537c7000c407ead1146d6b8fe9672bd
Mr. Sundance <sundancekid1108@gmail.com>
2018-12-29 18:35:47 +0900

    card design & vscode workspace setting

--- react-todo/App.js
+++ react-todo/App.js
@@ -4,17 +4,18 @@ import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 import { TextInput } from 'react-native-gesture-handler';
 
-const {height, width} = Dimensions.get("window")
+const { height, width } = Dimensions.get("window")
 
 export default class App extends React.Component {
     render() {
-        return ( <View style = { styles.container }>
-            <StatusBar barStyle = "light-content" />
-            <Text style={styles.title}>Nice ToDo</Text>
-            <View style={styles.card}>
-                <TextInput style={styles.input} placeholder={"New To Do"} />
+        return ( 
+            <View style = { styles.container }>
+                <StatusBar barStyle = "light-content" / >
+                    <Text style = { styles.title } > Nice ToDo </Text>
+                        <View style = { styles.card } >
+                            <TextInput style = { styles.input } placeholder = { "New To Do" } />
+                        </View>
             </View>
-        </View>
         );
     }
 }
@@ -31,14 +32,28 @@ const styles = StyleSheet.create({
         marginTop: 50,
         fontWeight: "400",
         marginBottom: 30
- 
+
     },
     card: {
         backgroundColor: "#FFF",
         flex: 1,
         width: width - 30,
         borderTopLeftRadius: 10,
-        borderTopRightRadius:10
-
+        borderTopRightRadius: 10,
+        ...Platform.select({
+            ios: {
+                shadowColor: "rgb(50, 50, 50)",
+                shadowOpacity: 0.5,
+                shadowRadius: 5,
+                shadowOffset: {
+                  height: -1,
+                  width: 0
+                }
+              },
+              android: {
+                elevation: 3
+              }
+        })
+       
     }
 });
